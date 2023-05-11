848d2d725cdd1aef3f7e46cd5d954281cbd145d7
Kris Santos <kris.santos101@gmail.com>
2023-05-11 00:16:04 -0600

    add a route to pass in the username to the urls_index template

--- express_server.js
+++ express_server.js
@@ -3,12 +3,15 @@ const generateRandomString = function(string) {
   return results;
 };
 const express = require("express");
+const cookieParser = require("cookie-parser")
 const app = express();
+
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -34,7 +37,10 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
